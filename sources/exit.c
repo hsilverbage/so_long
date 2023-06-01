fe970c1e8527322e5ba993a96002216f3f934819
Henrik Silverbage <hsilverb@z3r9p1.42lyon.fr>
2023-06-01 18:17:12 +0200

    free is going better but lets call it a day

--- sources/exit.c
+++ sources/exit.c
@@ -6,35 +6,103 @@
 /*   By: hsilverb <hsilverb@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 20:11:28 by hsilverb          #+#    #+#             */
-/*   Updated: 2023/06/01 16:40:25 by hsilverb         ###   ########lyon.fr   */
+/*   Updated: 2023/06/01 18:16:42 by hsilverb         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/so_long.h"
 
+// int	ft_close_mlx(t_game *game)
+// {
+// 	int	i;
+
+// 	i = game->height - 1;
+// 	mlx_destroy_window(game->mlx_ptr, game->win_ptr);
+// 	mlx_destroy_display(game->mlx_ptr);
+// 	while (i >= 0)
+// 	{
+// 		free(game->map[i]);
+// 		ft_printf("map line %d is freed\n", i);
+// 		i--;
+// 	}
+// 	free(game->map);
+// 	free(game->mlx_ptr);
+// 	// free(game->homer->img_ptr);
+// 	// free(game->wall->img_ptr);
+// 	// free(game->exit->img_ptr);
+// 	// free(game->exit_w_homer->img_ptr);
+// 	// free(game->donuts->img_ptr);
+// 	// free(game->grass->img_ptr);
+// 	// free(game->homer);
+// 	// free(game->wall);
+// 	// free(game->exit);
+// 	// free(game->exit_w_homer);
+// 	// free(game->donuts);
+// 	// free(game->grass);
+
+// 	exit (0);
+// }
+//Free so_long
+
+void	ft_free_map(char **str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str && str[i])
+	{
+		free(str[i]);
+		i++;
+	}
+	if (str)
+		free(str);
+}
+void	ft_destory_image(t_game *game)
+{
+	if (game) //&& CREATE AN INT THAT CHANGES ONCE I VE CREATED THE SPRITES
+	{
+		if (game->homer.img_ptr)
+			mlx_destroy_image(game->mlx_ptr, game->homer.img_ptr);
+		if (game->exit.img_ptr)
+			mlx_destroy_image(game->mlx_ptr, game->exit.img_ptr);
+		if (game->donuts.img_ptr)
+			mlx_destroy_image(game->mlx_ptr, game->donuts.img_ptr);
+		if (game->grass.img_ptr)
+			mlx_destroy_image(game->mlx_ptr, game->grass.img_ptr);
+		if (game->wall.img_ptr)
+			mlx_destroy_image(game->mlx_ptr, game->wall.img_ptr);
+		if (game->exit_w_homer.img_ptr)
+			mlx_destroy_image(game->mlx_ptr, game->exit_w_homer.img_ptr);
+	}
+}
+
+void	ft_free_all(t_game *env)
+{
+	ft_destory_image(game);
+	if (game->homer.img_path)
+		free(game->homer.img_path);
+	if (game->map.img_path)
+		free(game->map.img_path);
+	if (game->map.img_path01)
+		free(game->map.img_path01);
+	if (game->enemy.img_path)
+		free(game->enemy.img_path);
+	if (game->exit.img_path)
+		free(game->exit.img_path);
+	if (game->item.img_path)
+		free(game->item.img_path);
+}
+
 int	ft_close_mlx(t_game *game)
 {
-	int	i;
-
-	i = game->height - 1;
-	mlx_destroy_window(game->mlx_ptr, game->win_ptr);
-	mlx_destroy_display(game->mlx_ptr);
-	while (i >= 0)
-		free(game->map[i--]);
-	free(game->map);
+	if (game->map)
+		ft_free_map(game->map);
+	ft_free_all(env);
+	if (game->mlx_ptr)
+		mlx_destroy_window(game->mlx_ptr, game->win_ptr);
+	//mlx_destroy_display(env->mlx.init);
 	free(game->mlx_ptr);
-	free(game->homer->img_ptr);
-	free(game->wall->img_ptr);
-	free(game->exit->img_ptr);
-	free(game->exit_w_homer->img_ptr);
-	free(game->donuts->img_ptr);
-	free(game->grass->img_ptr);
-	free(game->homer);
-	free(game->wall);
-	free(game->exit);
-	free(game->exit_w_homer);
-	free(game->donuts);
-	free(game->grass);
-
-	exit (0);
+	exit(0);
+	return (0);
 }
+
